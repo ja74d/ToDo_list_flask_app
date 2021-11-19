0f6edbc715ad534362a14559a0c42f5e9e97a1b1
javad pournosrat <pournosrat.javad@gmail.com>
2021-11-19 16:32:34 +0330

    add database

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+app.secret_key = 'mysecretkey'
+
+#tasks form
+class Tasks:
+    task = db.Column(db.String(255), nullable=False)
+    submit = SubmitField('Add')
+
+
 @app.route('/')
 def index():
-    return "hello javad"
+    return render_template('index.html')
